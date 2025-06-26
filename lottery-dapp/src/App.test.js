0764bc6f5be00ff80072e96b212f315e235a4f10
Isaac Essuman <isaacessuman@gmail.com>
2025-06-25 21:20:25 -0400

    update App test to check main UI elements

--- lottery-dapp/src/App.test.js
+++ lottery-dapp/src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders main UI elements', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headings = screen.queryAllByText(/Lottery DApp/i);
+  const connectButtons = screen.queryAllByRole('button', {
+    name: /Connect Wallet/i,
+  });
+  expect(headings.length > 0 || connectButtons.length > 0).toBe(true);
 });
